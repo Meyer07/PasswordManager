cf5f2fb0c0bfcc32b6baec0001fe30c149e9e4c7
Sam Meyer <smeyer1@new.rr.com>
2026-02-08 23:12:27 -0600

    storage.js change

--- src/services/storage.js
+++ src/services/storage.js
@@ -1,29 +1,39 @@
 const storageService = {
-    KEYS: {
-      MASTER_HASH: 'pm_master_hash_v2',
-      ENCRYPTED_DATA: 'pm_encrypted_passwords_v2'
-    },
-  
-    saveMasterHash: (hash) => {
-      localStorage.setItem(storageService.KEYS.MASTER_HASH, hash);
-    },
-  
-    getMasterHash: () => {
-      return localStorage.getItem(storageService.KEYS.MASTER_HASH);
-    },
-  
-    saveEncryptedPasswords: (encryptedData) => {
-      localStorage.setItem(storageService.KEYS.ENCRYPTED_DATA, encryptedData);
-    },
-  
-    getEncryptedPasswords: () => {
-      return localStorage.getItem(storageService.KEYS.ENCRYPTED_DATA);
-    },
-  
-    clearAll: () => {
-      localStorage.removeItem(storageService.KEYS.MASTER_HASH);
-      localStorage.removeItem(storageService.KEYS.ENCRYPTED_DATA);
-    }
-  };
-  
-  export default storageService;
+  KEYS: {
+    MASTER_HASH: 'pm_master_hash_v2',
+    ENCRYPTED_DATA: 'pm_encrypted_passwords_v2',
+    RECOVERY_HASH: 'pm_recovery_hash_v2'
+  },
+
+  saveMasterHash: (hash) => {
+    localStorage.setItem(storageService.KEYS.MASTER_HASH, hash);
+  },
+
+  getMasterHash: () => {
+    return localStorage.getItem(storageService.KEYS.MASTER_HASH);
+  },
+
+  saveEncryptedPasswords: (encryptedData) => {
+    localStorage.setItem(storageService.KEYS.ENCRYPTED_DATA, encryptedData);
+  },
+
+  getEncryptedPasswords: () => {
+    return localStorage.getItem(storageService.KEYS.ENCRYPTED_DATA);
+  },
+
+  clearAll: () => {
+    localStorage.removeItem(storageService.KEYS.MASTER_HASH);
+    localStorage.removeItem(storageService.KEYS.ENCRYPTED_DATA);
+    localStorage.removeItem(storageService.KEYS.RECOVERY_HASH);
+  },
+
+  saveRecoveryHash: (encryptedHash) => {
+    localStorage.setItem(storageService.KEYS.RECOVERY_HASH, encryptedHash);
+  },
+
+  getRecoveryHash: () => {
+    return localStorage.getItem(storageService.KEYS.RECOVERY_HASH);
+  }
+};
+
+export default storageService;
